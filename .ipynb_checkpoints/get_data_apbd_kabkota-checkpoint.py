1b95fe02635daba3e0b77f3ed7967f1bbd307f12
Beta Uliansyah <betau@DESKTOP-4P02I5K.kemenkeu.go.id>
2025-08-07 13:48:20 +0700

    7 Agustus 2025

--- .ipynb_checkpoints/get_data_apbd_kabkota-checkpoint.py
+++ .ipynb_checkpoints/get_data_apbd_kabkota-checkpoint.py
@@ -6,23 +6,27 @@ from time import perf_counter
 # Start time for performance measurement
 start_time = perf_counter()
 tahun = 2025
-print('Bismillah. Perkiraan berjalan 4 detik. Mohon tunggu.')
+print('Bismillah. Perkiraan berjalan 54 detik. Mohon tunggu.')
 
 # Disable SSL warnings and initialize session
 requests.packages.urllib3.disable_warnings()
 s = requests.Session()
 
 # ambil data kode_provinsi
-provinsi_df = pd.read_csv('kode_provinsi.csv', dtype=object)
+kabkota_df = pd.read_csv('kode_kabkota.csv', dtype=object)
 
 # Fetch the webpage and collect data as a list of dictionaries
 data_list = []
 
-for kodepemda in provinsi_df['kodepemda']:
-    r = s.get(f'https://jaga.id/api/v5/kemenkeu-apbd/get-apbd?tipe=apbd&tahun={tahun}&kodewilayah={kodepemda}', verify=False)
+# make tuple pairs of kodewilayah and kodepemdaa
+pemda_list = []
+for kodepemda in kabkota_df['kodepemda']:
+    pemda_list.append((kabkota_df.query(f'kodepemda == "{kodepemda}"')['kodewilayah'].values[0], kodepemda))
+for kodewilayah, kodepemda in pemda_list:
+    r = s.get(f'https://jaga.id/api/v5/kemenkeu-apbd/get-apbd?tipe=apbd&tahun={tahun}&kodewilayah={kodewilayah}&kodesubwilayah={kodepemda}', verify=False)
     response_json = r.json()
-    namapemda = provinsi_df.query(f'kodepemda == "{kodepemda}"')['namapemda'].values[0]
-    provinsi = provinsi_df.query(f'kodepemda == "{kodepemda}"')['provinsi'].values[0]
+    namapemda = kabkota_df.query(f'kodepemda == "{kodepemda}"')['namapemda'].values[0]
+    provinsi = kabkota_df.query(f'kodepemda == "{kodepemda}"')['provinsi'].values[0]
     for i in response_json['data']:
         for j in response_json['data'][i]['level2']:
             for k in response_json['data'][i]['level2'][j]['level3']:
@@ -47,7 +51,7 @@ for kodepemda in provinsi_df['kodepemda']:
 data_df = pd.DataFrame(data_list)
 
 # Save the results to CSV
-data_df.to_csv(f'data_apbd_provinsi_level3-{tahun}.csv', index=False)
+data_df.to_csv(f'data_apbd_kabkota_level3-{tahun}.csv', index=False)
 
 # Calculate and print elapsed time
 end_time = perf_counter()
